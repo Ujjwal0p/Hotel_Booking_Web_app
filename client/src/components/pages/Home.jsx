ff1790b61f9ce02cf8f23e26cf73a91682800d28
upati01 <upatidar7697@gmail.com>
2025-05-23 12:52:35 +0530

    initial commit

---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../Hero'
+import FeaturedDestination from '../FeaturedDestination'
+import ExclusiveOffers from '../ExclusiveOffers'
+import Testimonial from '../Testimonial'
+import NewsLetter from '../NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <FeaturedDestination />
+        <ExclusiveOffers />
+        <Testimonial />
+        <NewsLetter />
+    </>
+  )
+}
+
+export default Home
